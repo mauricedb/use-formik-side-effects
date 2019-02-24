557621c2813bd34b4143866f9659bb43c1a40b22
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-02-24 08:24:10 +0100

    Minimal React example with Parcel

---
+++ example/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const ShapeEditor = () => {
+  return (
+    <div>
+      <h2>Shape Editor</h2>
+    </div>
+  );
+};
+
+ReactDOM.render(<ShapeEditor />, document.getElementById('root'));
